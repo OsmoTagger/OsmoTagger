8478cea13476941ebbe0545c6d54934c18111b78
Arkadii Seniiants <arkadii.seniiants@globus.software>
2023-09-21 01:14:52 +0300

    add presets to line tools

--- line tools/main.swift
+++ line tools/main.swift
@@ -16,12 +16,12 @@ Parser().fillChunks()
 
 print(AppSettings.settings.categories.count)
 print(AppSettings.settings.chunks.count)
-let data = try! Data(contentsOf: osmFilePath)
-let parser = XMLParser(data: data)
-let parserDelegate = OSMXmlParser()
-parser.delegate = parserDelegate
-parser.parse()
-print(parserDelegate.objects.count)
-AppSettings.settings.inputObjects = parserDelegate.objects
-
-print(AppSettings.settings.inputObjects)
+//let data = try! Data(contentsOf: osmFilePath)
+//let parser = XMLParser(data: data)
+//let parserDelegate = OSMXmlParser()
+//parser.delegate = parserDelegate
+//parser.parse()
+//print(parserDelegate.objects.count)
+//AppSettings.settings.inputObjects = parserDelegate.objects
+//
+//print(AppSettings.settings.inputObjects)
