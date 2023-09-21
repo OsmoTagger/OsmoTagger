b53b8b1e079af2e30329bedd3070970742ee8ea0
Arkadii Seniiants <arkadii.seniiants@globus.software>
2023-09-21 13:30:44 +0300

    add presetsClient in Line tools

--- line tools/main.swift
+++ line tools/main.swift
@@ -7,21 +7,3 @@
 
 import Foundation
 
-
-let osmFilePath = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first!.appendingPathComponent("kaliningrad-latest.osm")
-
-
-Parser().fillPresetElements()
-Parser().fillChunks()
-
-print(AppSettings.settings.categories.count)
-print(AppSettings.settings.chunks.count)
-//let data = try! Data(contentsOf: osmFilePath)
-//let parser = XMLParser(data: data)
-//let parserDelegate = OSMXmlParser()
-//parser.delegate = parserDelegate
-//parser.parse()
-//print(parserDelegate.objects.count)
-//AppSettings.settings.inputObjects = parserDelegate.objects
-//
-//print(AppSettings.settings.inputObjects)
